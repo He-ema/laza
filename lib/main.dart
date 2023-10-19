872fc6496a0fbcf83dcf38454191991feb2bb836
ibrahim Osama <zx.hema72@gmail.com>
2023-10-19 12:12:56 +0300

    transparent status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:laza/core/shared_cubits/cubit/auth_cubit.dart';
 import 'package:laza/core/theme/theme_cubit/theme_cubit.dart';
 import 'package:laza/core/utils/app_router.dart';
@@ -11,6 +12,10 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
